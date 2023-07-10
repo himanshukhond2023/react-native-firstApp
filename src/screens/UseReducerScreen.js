6e4c7cd08782ec036ebeee52913b4b07e43f5583
himanshukhond <himanshu.k@codearray.tech>
2023-07-10 13:01:07 +0530

    multiple useReducer

--- src/screens/UseReducerScreen.js
+++ src/screens/UseReducerScreen.js
@@ -20,10 +20,10 @@ import { TouchableOpacity, StyleSheet, View, Text } from "react-native";
 
 //     return (
 //         <View style={styles.container}>
+//             <Text style={styles.text}>Count: {count}</Text>
 //             <TouchableOpacity style={styles.button} onPress={() => dispatch("increment")}>
 //                 <Text style={styles.text}>Increment</Text>
 //             </TouchableOpacity>
-//             <Text style={styles.text}>Count: {count}</Text>
 //             <TouchableOpacity style={styles.button} onPress={() => dispatch("decrement")}>
 //                 <Text style={styles.text}>Decrement</Text>
 //             </TouchableOpacity>
@@ -36,20 +36,73 @@ import { TouchableOpacity, StyleSheet, View, Text } from "react-native";
 
 
 // useReducer (complex state and action)
-const initialState = {
-    firstCounter: 0,
-    secondCounter: 10
-};
+// const initialState = {
+//     firstCounter: 0,
+//     secondCounter: 10
+// };
+// const reducer = (state, action) => {
+//     switch(action.type) {
+//         case "increment":
+//             return {...state, firstCounter: state.firstCounter + action.value};
+//         case "decrement":
+//             return {...state, firstCounter: state.firstCounter - action.value};
+//         case "increment2":
+//             return {...state, secondCounter: state.secondCounter + action.value};
+//         case "decrement2":
+//             return {...state, secondCounter: state.secondCounter - action.value};
+//         case "reset":
+//             return initialState;
+//         default:
+//             return state;
+//     }
+// }
+// const UseReducerScreen = () => {
+//     const [count, dispatch] = useReducer(reducer, initialState);
+
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.text}>First Counter: {count.firstCounter}</Text>
+//             <Text style={styles.text}>Second Counter: {count.secondCounter}</Text>
+
+//             {/* First Counter */}
+//             <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "increment", value: 1})}>
+//                 <Text style={styles.text}>Increment</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "decrement", value: 1})}>
+//                 <Text style={styles.text}>Decrement</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "increment", value: 5})}>
+//                 <Text style={styles.text}>Increment by 5</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "decrement", value: 5})}>
+//                 <Text style={styles.text}>Decrement by 5</Text>
+//             </TouchableOpacity>
+
+//             {/* Second Counter */}
+//             <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "increment2", value: 1})}>
+//                 <Text style={styles.text}>Increment Counter 2</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "decrement2", value: 1})}>
+//                 <Text style={styles.text}>Decrement Counter 2</Text>
+//             </TouchableOpacity>
+
+//             {/* Reset Button for both the counter */}
+//             <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "reset"})}>
+//                 <Text style={styles.text}>Reset</Text>
+//             </TouchableOpacity>
+//         </View>
+//     );
+// }
+
+
+// Multiple useReducers
+const initialState = 0;
 const reducer = (state, action) => {
-    switch(action.type) {
+    switch(action) {
         case "increment":
-            return {...state, firstCounter: state.firstCounter + action.value};
+            return state + 1;
         case "decrement":
-            return {...state, firstCounter: state.firstCounter - action.value};
-        case "increment2":
-            return {...state, secondCounter: state.secondCounter + action.value};
-        case "decrement2":
-            return {...state, secondCounter: state.secondCounter - action.value};
+            return state - 1;
         case "reset":
             return initialState;
         default:
@@ -59,38 +112,35 @@ const reducer = (state, action) => {
 
 const UseReducerScreen = () => {
     const [count, dispatch] = useReducer(reducer, initialState);
+    const [countTwo, dispatchTwo] = useReducer(reducer, initialState);
 
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>First Counter: {count.firstCounter}</Text>
-            <Text style={styles.text}>Second Counter: {count.secondCounter}</Text>
-
-            {/* First Counter */}
-            <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "increment", value: 1})}>
+            {/* Count One */}
+            <Text style={styles.text}>Count One: {count}</Text>
+            <TouchableOpacity style={styles.button} onPress={() => dispatch("increment")}>
                 <Text style={styles.text}>Increment</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "decrement", value: 1})}>
+            <TouchableOpacity style={styles.button} onPress={() => dispatch("decrement")}>
                 <Text style={styles.text}>Decrement</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "increment", value: 5})}>
-                <Text style={styles.text}>Increment by 5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "decrement", value: 5})}>
-                <Text style={styles.text}>Decrement by 5</Text>
-            </TouchableOpacity>
-
-            {/* Second Counter */}
-            <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "increment2", value: 1})}>
-                <Text style={styles.text}>Increment Counter 2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "decrement2", value: 1})}>
-                <Text style={styles.text}>Decrement Counter 2</Text>
-            </TouchableOpacity>
-
-            {/* Reset Button for both the counter */}
-            <TouchableOpacity style={styles.button} onPress={() => dispatch({type: "reset"})}>
+            <TouchableOpacity style={styles.button} onPress={() => dispatch("reset")}>
                 <Text style={styles.text}>Reset</Text>
             </TouchableOpacity>
+
+            {/* Count Two */}
+            <View style={styles.container}>
+                <Text style={styles.text}>Count Two: {countTwo}</Text>
+                <TouchableOpacity style={styles.button} onPress={() => dispatchTwo("increment")}>
+                    <Text style={styles.text}>Increment</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => dispatchTwo("decrement")}>
+                    <Text style={styles.text}>Decrement</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={() => dispatchTwo("reset")}>
+                    <Text style={styles.text}>Reset</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -106,7 +156,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         backgroundColor: "white",
         paddingHorizontal: 20,
-        marginVertical: 20,
+        marginVertical: 10,
         justifyContent: "center",
         fontSize: 1000,
         fontWeight: "bold"
